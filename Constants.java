f90fdbfc784e3daed8da49faf0016612b320d225
caiqiqi <564933437@qq.com>
2015-11-01 05:58:04 +0800

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,1 +1,9 @@
-
+public interface Constants {  
+      
+    public final static String RECEIVE_FILE_PATH = "E:\\receive\\";  
+      
+    public final static String SEND_FILE_PATH = "E:\\send";  
+      
+    public final static int DEFAULT_BIND_PORT = 10000;  
+  
+}  
